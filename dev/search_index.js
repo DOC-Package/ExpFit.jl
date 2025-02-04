d97e26aa1b1ce52cfc1eacecd9d79e1fb70aec42
Documenter.jl <documenter@juliadocs.github.io>
2025-02-04 10:16:28 +0000

    build based on 443b9ef

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ExpFit","category":"page"},{"location":"#ExpFit","page":"Home","title":"ExpFit","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ExpFit.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ExpFit]","category":"page"}]
+}
